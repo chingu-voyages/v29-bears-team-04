a32548853f12ba34547516dd54f6e95c0f1c38a8
zkobrinsky <zachkobrinsky@gmail.com>
2021-04-12 21:56:07 -0400

    added search text event handler

--- unsplash-clone/src/components/header/SearchBar.tsx
+++ unsplash-clone/src/components/header/SearchBar.tsx
@@ -1,13 +1,19 @@
 import Search from "../../images/search-line.svg";
+import { useState } from 'react';
 
 export default function SearchBar() {
+    const [searchText, updateSearchText] = useState("");
+
+    const changeText = (e:any)=> {
+        updateSearchText(e.target.value)
+    }
     return (
         <>
         <form className="form-container">
             <button className="search-btn">
                 <img src={Search}></img>
             </button>
-            <input className="search-input"></input>
+            <input placeholder="search" value= { searchText } className="search-input" onChange={ changeText }></input>
         </form>
         </>
     );

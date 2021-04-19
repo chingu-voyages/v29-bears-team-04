b3d0dc259d8e1e44b7c0048bad2dddd02cb000c3
Cesar <cesdia883@gmail.com>
2021-04-18 23:46:54 -0400

    started login

--- unsplash-clone/src/components/generalComponents/header/Hamburger.tsx
+++ unsplash-clone/src/components/generalComponents/header/Hamburger.tsx
@@ -1,43 +1,61 @@
 import "./Header.scss";
 import Burger from "../../../images/hamburger-menu.svg";
-import { useState } from 'react'
-// import { Link } from 'react-router-dom'
-import DropdownItems from './DropdownItems';
-import DropdownItem from './DropdownItem'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
+const DropdownItems = [
+    {
+        title: "Home",
+        path: "/",
+    },
+
+    {
+        title: "Join",
+        path: "/join",
+    },
+
+    {
+        title: "Login",
+        path: "/login",
+    },
+
+    {
+        title: "Topics",
+        path: "/t/",
+    },
+];
 
 export default function Hamburger() {
-    const [dropdownState, setDropdownState] = useState(false)
+    const [dropdownState, setDropdownState] = useState<Boolean>(false);
 
-    const [loggedIn, setLoggedIn] = useState(false)
+    const toggleDropdown = (e: React.MouseEvent<any>) => setDropdownState(!dropdownState);
 
-    const toggleDropdown = (e:React.MouseEvent<any>) => setDropdownState(!dropdownState);
+    const closeMobileMenu = (e: React.MouseEvent<any>) => setDropdownState(false);
 
-    const closeMobileMenu = (e:React.MouseEvent<any>) => setDropdownState(false);
-
-  
-
-    
-
-
-
-    
     return (
         <>
-        <div className="hamburger" >
-        <div className={ dropdownState ? "dropdown active" : "dropdown"}>
-            <button className="burger-container" onClick={ toggleDropdown }>
-                <img src={Burger} alt="dropdown icon">
-
-                </img>
-            </button>
-                <ul >
-                    {dropdownState? DropdownItems.map((item, index) => {
-                        return <DropdownItem {...item} closeMobileMenu={closeMobileMenu} key={index}/>
-                    }) : null}
-                </ul>
+            <div className="hamburger">
+                <div className={dropdownState ? "dropdown active" : "dropdown"}>
+                    <button className="burger-container" onClick={toggleDropdown}>
+                        <img src={Burger} alt="dropdown icon"></img>
+                    </button>
+                    <ul>
+                        {dropdownState
+                            ? DropdownItems.map((item, index) => (
+                                  <li>
+                                      <Link
+                                          key={index}
+                                          to={item.path}
+                                          onClick={closeMobileMenu}
+                                      >
+                                          {item.title}
+                                      </Link>
+                                  </li>
+                              ))
+                            : null}
+                    </ul>
+                </div>
             </div>
-        </div>
         </>
-    )
+    );
 }
--- unsplash-clone/src/components/login/Login.tsx
+++ unsplash-clone/src/components/login/Login.tsx
@@ -1,5 +1,28 @@
-export default function Login(){
+import "./login.scss";
+import UnsplashLogo from "../generalComponents/images/UnsplashLogo";
+import FacebookLogo from "../generalComponents/images/FacebookLogo";
+
+export default function Login() {
     return (
-        <div>Hello from Login</div>
-    )
+        <div className="flex flex-container flex-v-center login-container">
+            <div className="login-container flex flex-col h-half">
+                <div className="flex flex-col flex-h-center upper">
+                    <div className="flex-h-center">
+                        <UnsplashLogo width={40} height={40} />
+                    </div>
+                    <div>Login</div>
+                    <div>Welcome back.</div>
+                </div>
+                <button className="facebook-login flex flex-h-center">
+                    <div >
+                        <FacebookLogo clas="facebook-logo" width={15} height={15} />
+                    </div>
+                    <div>Login with Facebook</div>
+                </button>
+                <form action="">
+                    <input type="text" />
+                </form>
+            </div>
+        </div>
+    );
 }

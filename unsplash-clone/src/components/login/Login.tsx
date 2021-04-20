cdf091ac2e420ce3c97001375d9c2aa4b781c5a7
Cesar <cesdia883@gmail.com>
2021-04-19 22:41:02 -0400

    more working

--- unsplash-clone/src/components/login/Login.tsx
+++ unsplash-clone/src/components/login/Login.tsx
@@ -5,20 +5,21 @@ import FacebookLogo from "../generalComponents/images/FacebookLogo";
 export default function Login() {
     return (
         <div className="flex flex-container flex-v-center login-container">
-            <div className="login-container flex flex-col h-half">
-                <div className="flex flex-col flex-h-center upper">
-                    <div className="flex-h-center">
-                        <UnsplashLogo width={40} height={40} />
+            <div className="login-container flex flex-col h-90 ">
+                <div className="flex flex-col flex-h-center upper pb-6">
+                    <div className="flex-h-center pb-5">
+                        <UnsplashLogo width={75} height={75} />
                     </div>
-                    <div>Login</div>
+                    <h2 className="login-title">Login</h2>
                     <div>Welcome back.</div>
                 </div>
-                <button className="facebook-login flex flex-h-center">
-                    <div >
+                <button className="facebook-login">
+                    <div className="flex-h-center">
                         <FacebookLogo clas="facebook-logo" width={15} height={15} />
                     </div>
                     <div>Login with Facebook</div>
                 </button>
+
                 <form action="">
                     <input type="text" />
                 </form>

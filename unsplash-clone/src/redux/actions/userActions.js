c39f01e2cd5ed8e0b1e198c593d4257551961b2f
zkobrinsky <zachkobrinsky@gmail.com>
2021-04-25 22:46:54 -0400

    removed dispatch warning

--- unsplash-clone/src/redux/actions/userActions.js
+++ unsplash-clone/src/redux/actions/userActions.js
@@ -1,15 +1,15 @@
 export const login = () => {
   return dispatch => {
-    fetch("https://unsplash-clone-server.herokuapp.com/login", {
+    fetch("https://unsplash-clone-server.herokuapp.com/users", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
     })
       .then(resp => resp.json())
-      // .then(resp => {
-      //   console.log(resp);
-      // })
+      .then(resp => {
+        console.log(resp);
+      })
       // .then(user => dispatch({ type: "LOGIN_SUCCESS", payload: user }))
       .catch(error => {
         console.log(error);

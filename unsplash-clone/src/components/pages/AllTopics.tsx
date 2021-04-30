7fc2e134437fb36f3743bb647b7f09b79a463c27
Cesar <cesdia883@gmail.com>
2021-04-30 18:05:30 -0400

    start of topics display

--- unsplash-clone/src/components/pages/AllTopics.tsx
+++ unsplash-clone/src/components/pages/AllTopics.tsx
@@ -1,3 +1,28 @@
+import React from "react";
+import TopicIntro from "../general/TopicIntro";
+
+import MockData from "../../MOCK_DATA.json";
+
 export default function AllTopics() {
-    return <div>All Topics Page</div>;
+    return (
+        <>
+            <TopicIntro
+                heading="Topics"
+                description="Explore the world through topics of beautiful photos free to use under the"
+            >
+                <p>
+                    <a href="#unsplash-license">Unsplash License</a>
+                </p>
+            </TopicIntro>
+            <h2>All Topics</h2>
+            {MockData.map((topic: any) => {
+                <div>
+                    <h1> {topic.topic}</h1>
+                    <p>{topic.description}</p>
+                </div>;
+            })}
+        </>
+    );
 }
+
+//TODO: Complete the flex grid and the display block

b7584fc57d3cb8f5891f6a120c6e198b4e0d135d
zkobrinsky <zachkobrinsky@gmail.com>
2021-05-05 15:52:39 -0400

    cookiehaschanged in useeffect only once

--- src/components/general/CookieHasChanged.tsx
+++ src/components/general/CookieHasChanged.tsx
@@ -4,6 +4,8 @@ export const cookieHasChanged = () => {
     let initCookies = Cookies.get();
     setInterval(() => {
         if (initCookies !== Cookies.get()) {
+            debugger;
+            console.log(`initCookies = ${initCookies}, Cookies.get() = ${Cookies.get()}`)
             console.log("cookie has changed")
             return true;
         } 
